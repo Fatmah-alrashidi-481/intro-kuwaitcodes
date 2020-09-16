36c57afd3be517eda3c93fe80516cafe50211174
Fatmah-alrashidi-C55 <66441802+Fatmah-alrashidi-481@users.noreply.github.com>
2020-09-16 20:47:38 +0300

    done

---
+++ class3/ios-cw3-p2.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func add(number1: Int , number2: Int ) -> Int {
+    return (number1 + number2)
+}
+var functionCall = add(number1: 3, number2: 5)
+print (functionCall)
---
+++ class3/ios-cw3.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+
+    
+func printMe (){
+    print("my name is fatmah alrashidi")
+    print("im 16 yers old")
+    print ("My favorite kind of animal is cat")
+}
+printMe()
+
